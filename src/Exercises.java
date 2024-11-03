1d8d75b7383cb42eefe33d2b1ffb02e75eb0ec40
MeelisBCS <meelis.teern@bcs.ee>
2024-11-04 00:09:41 +0200

    Template

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,7 +3,12 @@ public class Exercises {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        char a = 'a';
+        String b = "abc";
+        int number = 9;
+        double numberWithCommaValue = 5.333;
+
+        System.out.println(numberWithCommaValue);
 
     }
 
