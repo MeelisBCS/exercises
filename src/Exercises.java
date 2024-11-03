ab06ff5750ee880685e1f00af96dd0ff44ba3594
MeelisBCS <meelis.teern@bcs.ee>
2024-11-04 00:13:04 +0200

    Hello World

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,12 +3,7 @@ public class Exercises {
 
     public static void main(String[] args) {
 
-        char a = 'a';
-        String b = "abc";
-        int number = 9;
-        double numberWithCommaValue = 5.333;
-
-        System.out.println(numberWithCommaValue);
+        System.out.println("Hello World");
 
     }
 
